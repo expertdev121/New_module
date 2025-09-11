7873e39b32f4a061acf78091ef7b0c18326b4708
vishal <jhavishal523@gmail.com>
2025-09-11 10:36:14 +0530

    .

--- components/contacts/contacts-table.tsx
+++ components/contacts/contacts-table.tsx
@@ -111,8 +111,8 @@ export default function ContactsTable() {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 0,
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
     }).format(amount);
   };
 
